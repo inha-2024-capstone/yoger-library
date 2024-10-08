5e62ee5d57bd721bcddc10b6be99764db7feb906
siwoo <k74415834@gmail.com>
2024-10-08 21:26:30 +0900

    FEAT: componentScan하도록 설정

---
+++ src/main/java/org/library/yogerLibrary/config/AutoConfig.java
@@ -0,0 +1,9 @@
+package org.library.yogerLibrary.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.library.yogerLibrary")
+public class AutoConfig {
+}
